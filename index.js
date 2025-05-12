092128977297faa948e421f86bf1ee424fccff49
Kunzal Khawaja Fahim <kunzalfahim04@gmail.com>
2025-05-12 18:58:51 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+ const homeBtn = document.getElementById('homeButton');
+    homeBtn.addEventListener('click', () => {
+      // Navigate back to the homepage of the portfolio
+      window.location.href = '/';
+    });
